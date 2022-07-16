809347cce84e629af3ce3b4f72bd67e9a92449ed
grexon-hack <jsarabialugo@gmail.com>
2022-07-16 01:51:08 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { SideBarComponent } from './components/side-bar/side-bar.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SideBarComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideFirestore(() => getFirestore())
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'nuevosmedios-407f6',
+    appId: '1:180665172785:web:4465fa92a20f6bb0a1e610',
+    storageBucket: 'nuevosmedios-407f6.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyAz8gjCg3XPR3izsbCxuUxHtFvlA-l2riA',
+    authDomain: 'nuevosmedios-407f6.firebaseapp.com',
+    messagingSenderId: '180665172785',
+  },
   production: true
 };
